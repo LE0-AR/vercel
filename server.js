ce85897f1b5a7bea50701f053fd339857973132f
LE0-AR <153234169+LE0-AR@users.noreply.github.com>
2024-08-28 15:37:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 // Configuración de CORS
 app.use(cors({
   origin: 'https://rudohszk7afwbc7tytbvaw.on.drv.tw', // Ajusta esto al dominio de tu aplicación
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
 }));
 
 // Middleware para manejar JSON y URL-encoded
@@ -18,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configuración de Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // Cambia esto según tu proveedor de correo
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -29,7 +31,6 @@ const transporter = nodemailer.createTransport({
 app.post('/send-email', (req, res) => {
   const { nombre, email, telefono, message } = req.body;
 
-  // Configuración del correo electrónico
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.RECIPIENT_EMAIL,
@@ -37,7 +38,6 @@ app.post('/send-email', (req, res) => {
     text: `Nombre: ${nombre}\nEmail: ${email}\nTeléfono: ${telefono}\nMensaje: ${message}`,
   };
 
-  // Enviar el correo electrónico
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error al enviar el correo:', error);
@@ -48,15 +48,12 @@ app.post('/send-email', (req, res) => {
   });
 });
 
-// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static('public'));
 
-// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('Servidor funcionando');
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
